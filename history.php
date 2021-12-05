0cbb68cbd724ece528194e41e091d56136847eba
coftbred <80310115+coftbred@users.noreply.github.com>
2021-12-05 09:18:04 +0700

    create history.php

---
+++ history.php
@@ -0,0 +1,90 @@
+<?php 
+    include 'config.php';
+    include 'includes/header.php'; 
+    include 'includes/profilebody.php'; 
+?>
+
+<div class="container">
+    <h1 class="text-white"><i class="fa fa-history" aria-hidden="true"></i> History</h1>
+    <hr>
+</div>
+
+<div class="manager">
+    <div class="container p-lg-4">
+    <div class="card wish-list mb-3">
+        <div class="card-body">
+          <div class="row mb-4">
+            <div class="col-md-5 col-lg-3 col-xl-3">
+              <div class="view zoom overlay z-depth-1 rounded mb-3 mb-md-0">
+                <img class="img-fluid w-100"
+                  src="https://cdn.tgdd.vn/Products/Images/42/213031/iphone-12-do-new-2-600x600.jpg" alt="Sample">
+                <a href="#!">
+                </a>
+              </div>
+            </div>
+            <div class="col-md-7 col-lg-9 col-xl-9">
+              <div>
+                <div class="d-flex justify-content-between">
+                  <div>
+                    <h5>Product Name</h5>
+                    <p>Author: <span class="author-name" style="color: #b23cfd;"><strong>Admin</strong></span></p>
+                    <p class="mb-3 text-muted text-uppercase small">Category: </p>
+                    <p class="mb-2 text-muted text-uppercase small">Price: </p>
+                    <p class="mb-3 text-muted text-uppercase small">Description: </p>
+                    <p class="mb-3 text-muted text-uppercase small">Date created: </p>  <!-- XUẤT NGÀY SẢN PHẨM ĐƯỢC TẠO -->
+                    <p class="mb-3 text-muted text-uppercase small">Date: </p> <!-- Ngày sản phẩm được mua -->
+                  </div>
+                  <div>
+                    <div class="def-number-input number-input safari_only mb-0 w-100">
+                    <p>Quantity</p>
+                      <input class="quantity" min="0" name="quantity" value="1" type="number">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr class="mb-4">
+          <div class="row mb-4">
+            <div class="col-md-5 col-lg-3 col-xl-3">
+              <div class="view zoom overlay z-depth-1 rounded mb-3 mb-md-0">
+                <img class="img-fluid w-100"
+                  src="https://cdn.tgdd.vn/Products/Images/42/213031/iphone-12-do-new-2-600x600.jpg" alt="Sample">
+                <a href="#!">
+                </a>
+              </div>
+            </div>
+            <div class="col-md-7 col-lg-9 col-xl-9">
+              <div>
+                <div class="d-flex justify-content-between">
+                  <div>                 
+                    <h5>Product Name</h5>
+                    <p>Author: <span class="author-name" style="color: #b23cfd;"><strong>Admin</strong></span></p>
+                    <p class="mb-3 text-muted text-uppercase small">Category: </p>
+                    <p class="mb-2 text-muted text-uppercase small">Price: </p>
+                    <p class="mb-3 text-muted text-uppercase small">Description: </p>
+                    <p class="mb-3 text-muted text-uppercase small">Date created: </p>  <!-- XUẤT NGÀY SẢN PHẨM ĐƯỢC TẠO -->
+                    <p class="mb-3 text-muted text-uppercase small">Date: </p> <!-- Ngày sản phẩm được mua -->
+                  </div>
+                  <div>
+                    <div class="def-number-input number-input safari_only mb-0 w-100">
+                    <p>Quantity</p>
+                      <input class="quantity" min="0" name="quantity" value="1" type="number">                 
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+</div>
+
+ 
+   
+<?php 
+    include 'includes/footer.php'; 
+?>
